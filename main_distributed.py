cfa2bd1e4804a5bedd162cd9d4970ae2d874768b
Joon1221 <joonkang940@gmail.com>
2024-04-07 14:04:02 -0700

    change file name

--- main_distributed.py
+++ main_distributed.py
@@ -10,7 +10,6 @@ import copy
 import time
 import yaml
 
-import torch.optim as optim
 from networks.efficientNetB0 import EfficientNetB0
 from networks.simpleCNN import SimpleCNN
 from networks.resnet50 import Resnet50
@@ -19,11 +18,8 @@ from dataloader.cifar10_dataset import CIFAR10Dataset
 from dataloader.dataloader import get_data_loaders
 from train.val import val
 from test.test import test
-from train.train import train
 
-from utils import LOG, LOG_AND_PRINT, train_model, get_model_parameters, average_model_parameters, apply_model_parameters, average_model_gradients, apply_averaged_parameters_and_gradients
-
-
+from utils import train_model, get_model_parameters, average_model_parameters, average_model_gradients, apply_averaged_parameters_and_gradients
 
 def main():
     start_time = time.time()
@@ -42,17 +38,8 @@ def main():
     model_name = configs.get('model_name')
     device_name = configs.get('device_name')
     data_path = configs.get('data_path')
-    logging_path = configs.get('logging_path')
 
     print(f"Using device: {device_name}")
-
-    # Get user input
-    while True:
-        id = int(input("Enter process id: "))
-        if id < 0 or id > num_partitions-1:
-            print(f"Enter an id between 0 and {num_partitions-1}")
-        else:
-            break
 
     # Define transforms
     transform = transforms.Compose([
@@ -71,13 +58,13 @@ def main():
 
     # num_partitions & base model
     if model_name == 'SimpleCNN':
-        model = SimpleCNN().to(device_name)
+        base_model = SimpleCNN().to(device_name)
     elif model_name == "Resnet50":
-        model = Resnet50().to(device_name)
+        base_model = Resnet50().to(device_name)
     elif model_name == "Resnet18":
-        model = Resnet18().to(device_name)
+        base_model = Resnet18().to(device_name)
     elif model_name == "enet0":
-        model = EfficientNetB0().to(device_name)
+        base_model = EfficientNetB0().to(device_name)
     else:
         print("Model not supported")
         exit()
@@ -95,110 +82,56 @@ def main():
 
     # Create a DataLoader for each partition
     train_loaders = [DataLoader(partition, batch_size=batch_size, shuffle=True) for partition in partitions]
-    train_loader = train_loaders[id-1]
 
-    # Print separator before starting
-    if id == 0: 
-        LOG_AND_PRINT(text=f"========================================", file_path=logging_path)
-
-    # Model & loss & optimizer
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
     for epoch in range(num_epochs):
-        LOG_AND_PRINT(f'epoch {epoch} started for process {id}', file_path=logging_path)
-        LOG_AND_PRINT(f'training started for process {id}', file_path=logging_path)
-        train(model, device_name, train_loader, optimizer, criterion, epoch)
+        processes = []
+        queues = []
 
-        LOG_AND_PRINT(f'validating started for process {id}', file_path=logging_path)
-        val(model, device_name, val_loader, criterion, epoch, data_path)
+        # model copys
+        models = [copy.deepcopy(base_model) for _ in range(num_partitions)]
 
-        LOG_AND_PRINT(text=f"Saving model_{id}_{epoch}.pth", file_path=logging_path)
-        torch.save(model.state_dict(), f'{data_path}output/model_{id}_{epoch}.pth')
+        print(f"validation {epoch}")
+        criterion = nn.CrossEntropyLoss()
+        p = Process(target=val, args=(base_model, device_name, val_loader, criterion, epoch, data_path))
+        p.start()
+        processes.append(p)
 
-        # The leader and must combine the model
-        if id == 0:
-            follower_models = [] 
-            for i in range(0, num_partitions):
-                LOG_AND_PRINT(text=f"Leader is waiting for model_{i}_{epoch}.pth", file_path=logging_path)
+        # starting processes
+        print(f"training {epoch}")
+        for i in range(num_partitions):
+            queue = Queue()
+            queues.append(queue)
+            p = Process(target=train_model, args=(models[i], train_loaders[i], queue, epoch, learning_rate, device_name))
+            p.start()
+            processes.append(p)
 
-                cur_model_path = f'{data_path}output/model_{i}_{epoch}.pth'
+        # join processes
+        for p in processes:
+            p.join()
 
-                # Busy wait until file is available
-                while not os.path.exists(cur_model_path):
-                    time.sleep(1)
-                LOG_AND_PRINT(text=f"Leader read model_{i}_{epoch}.pth", file_path=logging_path)
+        # getting result from queues
+        gradients = []
+        for q in queues:
+            gradients.append(q.get())
 
-                # Read and save the model
-                temp_model = SimpleCNN().to(device_name)
-                temp_model.load_state_dict(torch.load(cur_model_path))
-                follower_models.append(temp_model)
+        # print(gradients)
+        avg_gradients = average_model_gradients(gradients)
 
-                # Delete the model after reading it
-                os.remove(cur_model_path)
+        all_model_parameters = [get_model_parameters(model) for model in models]
+        avg_parameters = average_model_parameters(all_model_parameters)
 
-            # Apply the averaged parameters to the leader model
-            all_model_parameters = [get_model_parameters(model) for model in follower_models]
-            avg_parameters = average_model_parameters(all_model_parameters)
-            # model = SimpleCNN().to(device_name)
-            apply_model_parameters(model, avg_parameters)
-            # optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-
-            # Save the model checkpoint
-            LOG_AND_PRINT(text=f"Saving model_combined_{epoch}.pth", file_path=logging_path)
-            torch.save(model.state_dict(), f'{data_path}output/model_combined_{epoch}.pth')
-
-            LOG_AND_PRINT(text=f"========================================", file_path=logging_path)
-        # The followers must wait for the combined model from the leader before proceeding
-        else: 
-            LOG_AND_PRINT(text=f"Follower {id} is waiting for model_combined_{epoch}.pth", file_path=logging_path)
-
-            cur_model_path = f"{data_path}output/model_combined_{epoch}.pth"
-
-            # Busy wait until file is available
-            while not os.path.exists(cur_model_path):
-                time.sleep(1)
-
-            LOG_AND_PRINT(text=f"Follower {id} read model_combined_{epoch}.pth", file_path=logging_path)
-
-            # Read and load the model
-            # model = SimpleCNN().to(device_name)
-            model.load_state_dict(torch.load(cur_model_path))
-            # optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-
-    # for epoch in range(num_epochs):
-    #     processes = []
-    #     queues = []
-
-    #     # model copys
-    #     # models = [copy.deepcopy(base_model) for _ in range(num_partitions)]
-
-
-    #     print(f"validation {epoch}")
-    #     criterion = nn.CrossEntropyLoss()
-    #     val(base_model, device_name, val_loader, criterion, epoch, data_path)
-
-    #     # starting processes
-    #     print(f"training {epoch}")
-    #     train_model(base_model, train_loaders[i], epoch, learning_rate, device_name)
-
-        # # print(gradients)
-        # avg_gradients = average_model_gradients(gradients)
-
-        # all_model_parameters = [get_model_parameters(model) for model in models]
-        # avg_parameters = average_model_parameters(all_model_parameters)
-
-        # apply_averaged_parameters_and_gradients(base_model, avg_parameters, avg_gradients)
+        apply_averaged_parameters_and_gradients(base_model, avg_parameters, avg_gradients)
 
         # optimizer = optim.Adam(base_model.parameters(), lr=learning_rate)
         # optimizer.step()
 
     # Test the model
     criterion = nn.CrossEntropyLoss()
-    test(model, device_name, test_loader, criterion, data_path)
+    test(base_model, device_name, test_loader, criterion, data_path)
 
     # Save the model checkpoint
-    torch.save(model.state_dict(), f'{data_path}/output/model_{id}_final.pth')
-    print('Finished Training. Model saved as model_{id}_final.pth.')
+    torch.save(base_model.state_dict(), f'{data_path}output/model.pth')
+    print('Finished Training. Model saved as model.pth.')
 
     end_time = time.time()
     print("Total Time: ", end_time-start_time)
