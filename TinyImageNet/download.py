7841ce7e70826fa0da953e597e0ab1a22d8b1830
Rayaq Siddiqui <rayaq05@hotmail.com>
2024-09-13 15:03:38 -0400

    initial steps of imagenet building

---
+++ TinyImageNet/download.py
@@ -0,0 +1,12 @@
+# from datasets import load_dataset
+
+
+# # Load dataset from Hugging Face Hub
+# dataset = load_dataset("zh-plus/tiny-imagenet", download_mode="force_redownload")
+# dataset.save_to_disk("./data")
+
+"""
+Download as follows
+git lfs install 
+git clone git@hf.co:datasets/zh-plus/tiny-imagenet
+"""
